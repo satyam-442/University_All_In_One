7b915069b9e15f6cac51cf44fdbf359a80170e26
zaheer <zaheeransari763@gmail.com>
2020-02-28 20:00:33 +0530

    gui update

--- app/src/main/java/com/example/sonoflordshiva/bnnaio/WelcomeActivity.java
+++ app/src/main/java/com/example/sonoflordshiva/bnnaio/WelcomeActivity.java
@@ -34,6 +34,7 @@ public class WelcomeActivity extends AppCompatActivity
             }
         });
 
+
         regis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
