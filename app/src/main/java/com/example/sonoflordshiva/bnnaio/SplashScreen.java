0a2f6d8b33e77b1a250280a23c7398d35f29fc99
zaheer <zaheeransari763@gmail.com>
2020-03-08 14:36:26 +0530

    gui update ver:1.0.3

---
+++ app/src/main/java/com/example/sonoflordshiva/bnnaio/SplashScreen.java
@@ -0,0 +1,46 @@
+package com.example.sonoflordshiva.bnnaio;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 5000;
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+
+       // image = findViewById(R.id.imageView);
+       // logo = findViewById(R.id.textView);
+       // slogan = findViewById(R.id.textView2);
+
+       // image.setAnimation(topAnim);
+       // slogan.setAnimation(bottomAnim);
+       // logo.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
