9d0323a685e4feebe92c5736b937d587a1b7b9a2
satyam-442 <satyamtiwari210@gmail.com>
2020-03-08 21:02:12 -0700

    Merge branch 'master' of D:\ApkProjects\BNNAIO with conflicts.

---
+++ app/src/main/java/com/example/sonoflordshiva/bnnaio/AsTeacher/LoginTeacher.java
@@ -0,0 +1,13 @@
+package com.example.sonoflordshiva.bnnaio.AsTeacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginTeacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_teacher);
+    }
+}
