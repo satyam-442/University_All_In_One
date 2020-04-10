390b02512952ae2006d2cfbf2c5a2c01a1005a4c
satyam-442 <satyamtiwari210@gmail.com>
2020-04-10 12:11:32 -0700

    project updation

--- app/src/main/java/com/example/sonoflordshiva/bnnaio/Feedback.java
+++ app/src/main/java/com/example/sonoflordshiva/bnnaio/Feedback.java
@@ -1,9 +1,16 @@
 package com.example.sonoflordshiva.bnnaio;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +21,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -25,6 +34,10 @@ import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Feedback extends AppCompatActivity {
@@ -34,9 +47,16 @@ public class Feedback extends AppCompatActivity {
     private NavigationView homeNavigationView;
     private TextView homeNavProfileName;
     FirebaseAuth mAuth;
-    DatabaseReference UserRef;
-    String currentUserId;
+    DatabaseReference UserRef,FeedbackRef;
+    String currentUserId, result, feedbackId;
     CircleImageView imageVHead;
+
+    EditText feedbackName, feedbackSpinnerValue, feedbackSpinnerOtherOption;
+    Spinner selectOptions;
+    Button sendButton;
+    String options[] = {"-Select Options-","Material Problem","User Interface","Service Problem","Others"};
+    ArrayAdapter <String> adapter;
+    ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +66,7 @@ public class Feedback extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         currentUserId = mAuth.getCurrentUser().getUid();
         UserRef= FirebaseDatabase.getInstance().getReference().child("Students");
-
+        FeedbackRef= FirebaseDatabase.getInstance().getReference().child("Feedback");
 
         mToolbar = (Toolbar) findViewById(R.id.feedback_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -62,6 +82,59 @@ public class Feedback extends AppCompatActivity {
         View navView = homeNavigationView.inflateHeaderView(R.layout.navigation_header);
         homeNavProfileName = (TextView) navView.findViewById(R.id.nav_user_full_name);
         imageVHead = (CircleImageView) navView.findViewById(R.id.nav_header_profile);
+
+        //FEEDBACK VALIDATIONS
+        feedbackName = findViewById(R.id.feedbackName);
+        feedbackSpinnerValue = findViewById(R.id.feedbackSpinnerValue);
+        feedbackSpinnerOtherOption = findViewById(R.id.feedbackSpinnerOtherOption);
+        selectOptions = findViewById(R.id.selectOptions);
+        loadingBar = new ProgressDialog(this);
+
+        sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveFeedbackToDatabase();
+            }
+        });
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,options);
+        selectOptions.setAdapter(adapter);
+        selectOptions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                switch (position)
+                {
+                    case 0:
+                        result = "-Select Options-";
+                        feedbackSpinnerValue.setText(result);
+                        break;
+                    case 1:
+                        result = "Material Problem";
+                        feedbackSpinnerValue.setText(result);
+                        break;
+                    case 2:
+                        result = "User Interface";
+                        feedbackSpinnerValue.setText(result);
+                        break;
+                    case 3:
+                        result = "Service Problem";
+                        feedbackSpinnerValue.setText(result);
+                        break;
+                    case 4:
+                        result = "Others";
+                        feedbackSpinnerValue.setText(result);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
+        });
 
         UserRef.child(currentUserId).addValueEventListener(new ValueEventListener()
         {
@@ -119,6 +192,69 @@ public class Feedback extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void SaveFeedbackToDatabase()
+    {
+        String name = feedbackName.getText().toString();
+        String spinnerValue = feedbackSpinnerValue.getText().toString();
+        String suggestion = feedbackSpinnerOtherOption.getText().toString();
+
+        final String saveCurrentTime, saveCurrentDate;
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        Calendar calForTime = Calendar.getInstance();
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss");
+        saveCurrentTime = currentTime.format(calForTime.getTime());
+
+        feedbackId = saveCurrentDate + saveCurrentTime;
+
+        if (TextUtils.isEmpty(name) && TextUtils.isEmpty(spinnerValue) && TextUtils.isEmpty(suggestion))
+        {
+            Toast.makeText(this, "Field's are empty!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setMessage("please wait...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(false);
+            HashMap<String,Object> feedbackMap = new HashMap<String,Object>();
+            feedbackMap.put("fullname", name);
+            feedbackMap.put("feedbackType" , spinnerValue);
+            feedbackMap.put("feedbackSuggestion",suggestion);
+            feedbackMap.put("uid",currentUserId);
+            feedbackMap.put("fbId",feedbackId);
+            feedbackMap.put("time",saveCurrentTime);
+            feedbackMap.put("date",saveCurrentDate);
+            /*feedbackMap.put("Contact",contact);
+            feedbackMap.put("UID",uid);
+            feedbackMap.put("Password",password);
+            feedbackMap.put("image","default");*/
+            FeedbackRef.child(spinnerValue).child(currentUserId).child(feedbackId).updateChildren(feedbackMap).addOnCompleteListener(new OnCompleteListener()
+            {
+                @Override
+                public void onComplete(@NonNull Task task)
+                {
+                    if(task.isSuccessful())
+                    {
+                        //SendUserToMainActivity();
+                        Toast.makeText(Feedback.this, "Feedback updated Successfully...", Toast.LENGTH_LONG).show();
+                        feedbackName.setText("");
+                        feedbackSpinnerValue.setText("");
+                        feedbackSpinnerOtherOption.setText("");
+                        loadingBar.dismiss();
+                    }
+                    else
+                    {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(Feedback.this, "Error Occurred:" + message, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
     }
 
     @Override
